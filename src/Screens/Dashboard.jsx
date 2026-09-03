db618782c4182da693191971300bcdc789ff1e90
kmr6702 <katy.richardson243@gmail.com>
2026-09-03 18:46:24 -0400

    Created Regexs that check for uppercase, lowercase, symbols, and numbers and updates the appropriate counts

--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -7,16 +7,47 @@ export default function Dashboard(){
     const [upperCaseCount, setUpperCaseCount] = useState(0); //A varaible to hold the amount of uppercase letters present in the password 
     const [lowerCaseCount, setLowerCaseCount] = useState(0); //A variable to hold the amount or lowercase letters present in the password 
     const [symbolCount, setSymbolCount] = useState(0);       //A variable to hold the amount of symbols present in the password 
+    const [numberCount, setNumberCount] = useState(0);       //A varaible to hold the amount of numbers present in the password 
 
+    const lowercase = /[a-z]/
+    const uppercase = /[A-Z]/
+    const number = /[0-9]/
+    const symbols = /[!@#$%^&*,.()-_+=/?]/
 
+    {/*Update the state everytime a change is made to the password input*/}
     const handleChange = (e) => {
-        setPassword(e.target.value);
-        setLength(e.target.value.length);
+        const newPassword = e.target.value;
+    
+        setPassword(newPassword);
+        setLength(newPassword.length);
+
+        {/*Place holders that count the number of each type of value*/}
+        let uppercase_temp = 0;
+        let lowercase_temp = 0;
+        let numbers_temp = 0;
+        let symbols_temp = 0;
+        for(let i = 0; i < newPassword.length; i++){
+            const char = newPassword[i];
+
+            if(lowercase.test(char)){
+                lowercase_temp++;
+            }else if(uppercase.test(char)){
+                uppercase_temp++;
+            }else if(number.test(char)){
+                numbers_temp++;
+            }else if(symbols.test(char)){
+                symbols_temp++;
+            }
+        }
+        setLowerCaseCount(lowercase_temp);
+        setUpperCaseCount(uppercase_temp);
+        setSymbolCount(symbols_temp);
+        setNumberCount(numbers_temp);
     }
 
     return(
         <>
-            {/*Take the users input, stores it in the password variable, and updates the state every time a change is made*/}
+            {/*Take the users input, stores it in the password variable*/}
             <label>
                 Enter your password here:
                 <input 
@@ -32,7 +63,8 @@ export default function Dashboard(){
                     <li>Uppercase Count: {upperCaseCount}</li>
                     <li>Lowercase Count: {lowerCaseCount}</li>
                     <li>Symbol Count: {symbolCount}</li>
-                    <li>Lenght: {length}</li>
+                    <li>Length: {length}</li>
+                    <li>Numbers: {numberCount}</li>
                 </ul>
             </p>
         </>
