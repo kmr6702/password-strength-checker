518885701b390e5b8aca36064e5d6396d0397396
kmr6702 <katy.richardson243@gmail.com>
2026-09-04 12:17:06 -0400

    Edited the about the developer page

--- src/Screens/AboutTheDev.jsx
+++ src/Screens/AboutTheDev.jsx
@@ -1,3 +1,20 @@
 export default function AboutTheDev(){
-    
+    return(
+        <>
+            <h1>About The Developer</h1>
+            <h3>Hi! I'm Katy</h3>
+
+            <header>About Me:</header>
+            <p>I gradauted for Penn State with my Bachelor of Science in Software Engineering with a minor in Game Development. Now I'm at Rochester Institute of Technology studying for a Master of 
+                Computer Science with a specialization in Cybersecurity and an Advanced Certificate in Artificial Intelligence.</p>
+
+            <header>Why I Built This:</header>
+            <p></p>
+
+            <header>What I'm Learning:</header>
+            <p></p>
+
+            
+        </>
+    );
 }
