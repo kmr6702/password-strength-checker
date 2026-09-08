f000d6947741f9c90867857ccad3f7dbb0bb3aec
kmr6702 <katy.richardson243@gmail.com>
2026-09-07 22:37:03 -0400

    Added a file of common words found within common passwords

---
+++ src/Analysis/commonWords.js
@@ -0,0 +1,16 @@
+export const commonWords =[
+    "admin",
+    "qwerty",
+    "password",
+    "abc",
+    "123",
+    "cba",
+    "321",
+    "asdf",
+    "qaz",
+    "wsx",
+    "zaq",
+    "xsw",
+    "guest",
+    "change"
+];
